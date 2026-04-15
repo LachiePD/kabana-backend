15040fab460813d0d6b4a499c65014579c2bdc5d
lachie <lachiepd@gmail.com>
2026-04-15 12:26:36 +1000

    refactor: removed misleading if statement, code should always be accessed

--- middleware/error.middleware.mjs
+++ middleware/error.middleware.mjs
@@ -1,9 +1,6 @@
 export const error = (error, req, res, next) => {
-  if (error) {
-    console.error(error);
-    return res
-      .status(error.code)
-      .json({ message: error.message, code: error.code });
-  }
-  next();
+  console.error(error);
+  return res
+    .status(error.code)
+    .json({ message: error.message, code: error.code });
 };
