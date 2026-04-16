1912117d0ba9da0941181fe509f6642eff7cf159
lachie <lachiepd@gmail.com>
2026-04-17 06:59:22 +1000

    refactor: test now more specific

--- tests/intergration/Repository.int.test.mjs
+++ tests/intergration/Repository.int.test.mjs
@@ -4,9 +4,14 @@ import { assert, Repository } from "#/repository/index.mjs";
 import { DbConnection } from "#/infrastructure/index.mjs";
 describe("repo integration with the backend", () => {
   it("returns data from the db", async () => {
-    const dbConnection = new DbConnection();
-    const repo = new Repository(dbConnection);
+    const db = new DbConnection();
+
+    await db.query("INSERT into projects (project_name) VALUES ($1)", [
+      "testProject",
+    ]);
+    const repo = new Repository(db);
     const data = await repo.getAllByEntity("projects");
-    expect(data).toHaveProperty("rows");
+    expect(data.rows[1].project_name).toEqual("testProject");
+    await db.cleanup();
   });
 });
