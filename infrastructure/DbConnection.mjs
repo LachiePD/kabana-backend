dac2281ad78c2d74bea75f1a60c9a9f89cf95ef5
lachie <lachiepd@gmail.com>
2026-04-16 13:19:19 +1000

    feature: seed() method, for tests

--- infrastructure/DbConnection.mjs
+++ infrastructure/DbConnection.mjs
@@ -11,8 +11,19 @@ export class DbConnection {
     return true;
   }
 
-  async query(query) {
-    const data = await this.pool.query(query);
+  async query(query, params) {
+    const data = await this.pool.query(query, params);
     return data;
+  }
+  async seed({ insert, select }) {
+    try {
+      await this.query(insert.query, insert.params || []);
+
+      const result = await this.query(select.query);
+
+      return result.rows;
+    } finally {
+      await this.query("TRUNCATE TABLE projects RESTART IDENTITY CASCADE");
+    }
   }
 }
