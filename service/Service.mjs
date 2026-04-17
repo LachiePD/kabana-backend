2e10a99b90be3a7583181ddad8b2d135f314b555
lachie <lachiepd@gmail.com>
2026-04-17 17:40:38 +1000

    feature: beginning the service implementation

--- service/Service.mjs
+++ service/Service.mjs
@@ -1,11 +1,18 @@
-class Service {
-  constructor(repo) {
+export class Service {
+  constructor(repo, type) {
     this.repo = repo;
+    this.type = type;
   }
-  getAllByEntity(entityType) {}
-  getByDomain(domainId) {}
-  createByDomain(data, domain) {}
-  deleteByDomain(domainId) {}
-  updateByDomain(domainId, data) {}
-  getByContext(contextId, domain) {}
+
+  getAllByEntity() {
+    return this.repo.getAllByEntity(this.type);
+  }
+
+  getByEntity(id) {
+    return this.repo.getByEntity(this.type, id);
+  }
+
+  deleteEntity(id) {
+    return this.repo.deleteEntity(this.type, id);
+  }
 }
