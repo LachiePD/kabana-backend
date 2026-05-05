ed28c706ac80e8eb53534d4edc9ed66d3b6d5da0
lachie <lachiepd@gmail.com>
2026-05-05 17:20:30 +1000

    remove: deleted dependency

--- repository/Account.repository.mjs
+++ repository/Account.repository.mjs
@@ -1,5 +1,3 @@
-import { buildInsertQuery } from "./buildInsertQuery.mjs";
-
 export class AccountRepository {
   constructor(dbConnection) {
     if (!dbConnection) throw new Error("invalid dbConnection");
@@ -16,40 +14,19 @@ export class AccountRepository {
   }
 
   async create(data) {
-	  const columns = Object.getOwnPropertyNames(data);
-	  const values = Object.values(data);
-	  const placeholders = values.map(_, i => `$${i+1}`).join(', ');
-	  const query = `INSERT INTO ${this.type} (${columns.join(', '})
+    const columns = Object.getOwnPropertyNames(data);
+    const values = Object.values(data);
+    const placeholders = values.map(_, (i) => `$${i + 1}`).join(", ");
+    const query = `INSERT INTO ${this.type} (${columns.join(", ")})
 	  VALUES (${placeholders})
-	  RETURNING *`	
+	  RETURNING *`;
     return this.dbConnection.query(query, values);
   }
 
-  async delete( id) {
+  async delete(id) {
     const query = `DELETE FROM "${this.type}" WHERE id = $1`;
     return await this.dbConnection.query(query, [id]);
   }
-  async updateByEntity(type, entityId, data) {
-    //TODO, write tests for this nonsense.
-    //three params is a lot for a method, we need to think about breaking this apart
-    const keys = Object.keys(data);
-    const values = Object.values(data);
-
-    const columns = keys
-      .map((key, index) => `${key} = $${index + 1}`)
-      .join(", ");
-
-    const finalValues = [...values, entityId];
-    const idPlaceholder = `$${finalValues.length}`;
-
-    const query = `
-    UPDATE ${type}
-    SET ${columns}
-    WHERE id = ${idPlaceholder}
-    RETURNING *; 
-  `;
-
-    return await this.dbConnection.query(query, finalValues);
-  }
+  async update(data) {}
   async getByContext(contextData, type) {}
 }
