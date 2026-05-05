6b502bd034ed3e7142dd611d4f1bbb8de1835118
lachie <lachiepd@gmail.com>
2026-05-05 17:07:43 +1000

    remove: unnecessary assertions/tests

--- repository/assert.mjs
+++
@@ -1,5 +0,0 @@
-export const assert = (entity) => {
-  const validEntities = ["projects", "developers", "issues", "accounts"];
-  if (!validEntities.includes(entity))
-    throw new Error(`invalid entity :    ${entity}`);
-};
