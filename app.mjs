68cc346704ce1ce3bf9dd03687ca5e8e8c197b37
lachie <lachiepd@gmail.com>
2026-05-06 21:35:32 +1000

    feature: /login' route

--- app.mjs
+++ app.mjs
@@ -6,6 +6,7 @@ import { DbConnection } from "#/infrastructure/index.mjs";
 import { requestHydrator } from "#/requestHydrator/index.mjs";
 import { AppError } from "#/error/AppError.mjs";
 import { Pool } from "pg";
+const NODE_ENV = process.env.NODE_ENV;
 const connectionString = process.env.CONNECTION_STRING;
 
 export const createApp = (routeDeps) => {
@@ -18,9 +19,20 @@ export const createApp = (routeDeps) => {
   app.use(express.json());
   app.post("/accounts", async (req, res) => {
     const safeInput = requestHydrator.createAccount(req.body);
-
     const id = await accountService.createAccount(safeInput);
     return res.status(200).json({ id });
+  });
+  app.post("/login", async (req, res) => {
+    const safeInput = requestHydrator.login(req.body);
+    const { token } = await accountService.login(safeInput);
+    return res
+      .status(200)
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: NODE_ENV === "test" ? false : true,
+        sameSite: "lax",
+      })
+      .json({ message: "Login successful" });
   });
   app.use(error);
   return app;
