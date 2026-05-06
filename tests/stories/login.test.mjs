43a306b4706fd02221fb2449fd9448d92ded6c0c
lachie <lachiepd@gmail.com>
2026-05-06 15:04:56 +1000

    test: login story

---
+++ tests/stories/login.test.mjs
@@ -0,0 +1,21 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import request from "supertest";
+import { createApp } from "#/app.mjs";
+import { setupDb } from "../utils/setupDb.mjs";
+
+describe("user logs in", () => {
+  let app;
+  beforeEach(async () => {
+    app = createApp();
+    await setupDb();
+    await request(app)
+      .post("/accounts")
+      .send({ name: "Lachie", password: "example123" });
+  });
+  it("returns a cookie setting response", async () => {
+    const account = { name: "Lachie", password: "example123" };
+    const response = await request(app).post("/login").send(account);
+    expect(response.status).toBe(200);
+    expect(response.headers["set-cookie"]).toBeDefined();
+  });
+});
